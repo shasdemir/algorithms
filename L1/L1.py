f2985289d5f32576c6b3791e8edfb3eb5b8fdf4e
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-10-16 00:47:35 +0300

    Countdown and time.

--- L1/L1.py
+++ L1/L1.py
@@ -1,6 +1,3 @@
-__author__ = 'sukruhasdemir'
-
-
 def russian(a, b):
     x = a; y = b
     z = 0
@@ -9,3 +6,20 @@ def russian(a, b):
         y = y << 1
         x = x >> 1
     return z
+
+
+def countdown(x):
+    y = 0
+    while x > 0:
+        x = x - 5
+        y = y + 1
+    print y
+    return y
+
+
+def time(n):
+    """ Return the number of steps necessary to calculate `print countdown(n)`"""
+
+    steps = 3 + 2 * (n / 5 + (n % 5 != 0))
+    return steps
+
