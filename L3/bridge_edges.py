9a32f633b7303a4f81040fcfaff54ba2af9e6b6d
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-11-04 22:19:28 +0200

    create_rooted_spanning_tree divided in two.

--- L3/bridge_edges.py
+++ L3/bridge_edges.py
@@ -1,7 +1,7 @@
 from L3 import *
 
 
-def make_colored_link(G, node1, node2, color=1):
+def make_colored_link(G, node1, node2, color):
     if node1 not in G:
         G[node1] = {node2: color}
     elif node2 not in G[node1]:
@@ -15,6 +15,37 @@ def make_colored_link(G, node1, node2, color=1):
     return G
 
 
-# def create_rooted_spanning_tree(G):
-#     colored = {}
+def create_uncolored_tree(G):
+    spanning_tree = {}
 
+    visited = set()
+    open_list = [G.keys()[0]]
+
+    while open_list:
+        current = open_list.pop(0)
+
+        if current not in spanning_tree:
+            spanning_tree[current] = {}
+
+        for neighbor in G[current]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                open_list.append(neighbor)
+
+                make_link(G=spanning_tree, node1=current, node2=neighbor)
+
+    return spanning_tree
+
+
+def create_rooted_spanning_tree(G):
+    spanning_tree = create_uncolored_tree(G)
+    colored_tree = {}
+
+    for node in G:
+        for nbor in G[node]:
+            if nbor in spanning_tree[node]:
+                make_colored_link(G=colored_tree, node1=node, node2=nbor, color='green')
+            else:
+                make_colored_link(G=colored_tree, node1=node, node2=nbor, color='red')
+
+    return colored_tree
