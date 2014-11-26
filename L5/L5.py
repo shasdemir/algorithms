32ad1dbfdb7811182d3080ac8ef41c0a75e3199c
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-11-26 21:19:56 +0200

    Started adding NamedHeap class.

--- L5/L5.py
+++ L5/L5.py
@@ -21,3 +21,20 @@ def dijkstra(G, v):
 
 def shortest_dist_node(dist):
     return min(dist.items(), key=lambda tup: tup[1])[0]  # Theta(n)
+
+
+class NamedHeap(object):
+    def __init__(self, heap_list, heaped_names):
+        self.heap_list = heap_list
+        self.heaped_names = heaped_names
+        self.name_mapping = dict(zip(heaped_names, heap_list))
+
+        self.down_heapify(self, 0)
+
+    def get_value(self, name):
+        return self.name_mapping[name]
+
+    def get_minimum(self):
+        return self.heaped_names[0], self.heap_list[0]
+
+
